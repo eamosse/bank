48c2ee2814c739068f166aac5403d1e2a1eace83
Amosse Edouard <Amosse Edouard@DESKTOP-N9EJKAS.lan>
2021-06-02 15:27:12 +0200

    First commit

--- src/java/entities/Adress.java
+++ src/java/entities/Adress.java
@@ -7,10 +7,6 @@ package entities;
 
 import java.io.Serializable;
 import javax.persistence.Embeddable;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
@@ -22,45 +18,9 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class Adress implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
     String numero;
     String rue; 
     String ville; 
     String codePostal;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Adress)) {
-            return false;
-        }
-        Adress other = (Adress) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "entities.Adress[ id=" + id + " ]";
-    }
-    
 }
